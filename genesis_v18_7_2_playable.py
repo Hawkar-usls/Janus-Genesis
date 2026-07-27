a89a3e36729a3e251bad23fc84e2bac76514a290
Hawkar-usls <hawkarlol@gmail.com>
2026-07-27 18:01:51 +0300

    feat: add explicit v18.7.2 playable alias

---
+++ genesis_v18_7_2_playable.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Explicit v18.7.2 import name for integrations that pin minor versions."""
+from __future__ import annotations
+
+from genesis_v18_7_playable import PLAYABLE_VERSION, PlayableGenesisV187
+
+
+class PlayableGenesisV1872(PlayableGenesisV187):
+    """Alias of the primary v18.7.2 runtime."""
