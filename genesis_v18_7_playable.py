8231db307e0b9d45010e84357d08b47cb9b43863
Hawkar-usls <hawkarlol@gmail.com>
2026-07-28 21:45:16 +0300

    Wire rupture voice integrity into primary runtime

--- genesis_v18_7_playable.py
+++ genesis_v18_7_playable.py
@@ -24,6 +24,7 @@ from genesis_v18_7_7 import BenevolentSovereignMixin
 from genesis_v18_7_7_voice_integrity import SovereignVoiceIntegrityMixin
 from genesis_v18_7_10 import BoundAssessorI0Mixin
 from genesis_v18_7_10_patch import BoundAssessorI0IntegrationPatchMixin
+from genesis_v18_7_10_rupture_voice import RuptureVoiceIntegrityMixin
 from genesis_v18_7_9_persistence import BoundAuthorityPersistenceMixin
 from genesis_v18_7_9_reactive_verifier import ReactiveBoundAuthorityVerifierMixin
 from genesis_v18_7_compat import GenesisV187CompatibilityMixin
@@ -49,6 +50,7 @@ class PlayableGenesisV187(
     GenesisV187CompatibilityMixin,
     ReactiveBoundAuthorityVerifierMixin,
     BoundAuthorityPersistenceMixin,
+    RuptureVoiceIntegrityMixin,
     BoundAssessorI0IntegrationPatchMixin,
     BoundAssessorI0Mixin,
     SovereignVoiceIntegrityMixin,
