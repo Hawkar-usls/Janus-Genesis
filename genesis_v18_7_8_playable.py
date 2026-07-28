731b1f71ad1f42a40f7651cef1ce12df72e5e315
Hawkar-usls <hawkarlol@gmail.com>
2026-07-28 04:20:59 +0300

    Add v18.7.8 playable alias

---
+++ genesis_v18_7_8_playable.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from genesis_v18_7_playable import PLAYABLE_VERSION, PlayableGenesisV187
+
+
+class PlayableGenesisV1878(PlayableGenesisV187):
+    """Alias of the primary Genesis v18.7.8 runtime."""
